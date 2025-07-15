39a4b87b01f16a1bf4bcf028dc950d8223604cf8
Ezekiel Leslie Omire <ezekielomire@gmail.com>
2025-07-15 05:16:44 +0300

    Updated App component to render ProjectList with project data

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
+// src/components/App.js
 import React from "react";
-import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import NavBar from "./NavBar";
 import ProjectList from "./ProjectList";
-import user from "../data/user";
+import projects from "../data/projects"; // assuming your data file is here
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <Home />
+      <About />
+      <ProjectList projects={projects} />
     </div>
   );
 }
