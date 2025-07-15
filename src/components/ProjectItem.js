cd9ea62015faf700face90d7d3c44f8ebebd7828
Ezekiel Leslie Omire <ezekielomire@gmail.com>
2025-07-15 05:14:56 +0300

    Added ProjectItem component to render project details and technologies with keys

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,16 @@
+// src/components/ProjectItem.js
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const techElements = technologies.map((tech) => (
+    <span key={tech}>{tech}</span>
+  ));
+
   return (
     <div className="project-item">
-      <h3>{name}</h3>
+      <h2>{name}</h2>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{techElements}</div>
     </div>
   );
 }
