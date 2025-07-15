87a8fc8b61f39596711efc8979072d0711e7fc16
Ezekiel Leslie Omire <ezekielomire@gmail.com>
2025-07-15 05:13:43 +0300

    Created NavBar component with dynamic links and unique key props

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,16 @@
+// src/components/NavBar.js
 import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkElements = links.map((link) => (
+    <a key={link} href={`#${link}`}>
+      {link}
+    </a>
+  ));
+
+  return <nav>{linkElements}</nav>;
 }
 
 export default NavBar;
